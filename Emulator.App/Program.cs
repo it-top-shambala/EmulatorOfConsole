e0bb331b1b65d3f6a4462fa94ece96e0d39e5b28
BairamovAllaz <ellezbayramov2004@gmail.com>
2022-06-18 23:27:06 +0300

    Added Emulator.App

---
+++ Emulator.App/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Emulator.App
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello Emulator App!");
+        }
+    }
+}
